c2178b09a13e77933231459980cd5f3350e39bca
Dede Firmansah <dedefirmansah0508@gmail.com>
2025-01-10 16:58:23 +0700

    add new endpoint for user & admin

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -20,11 +20,12 @@ func NewAuthController(authUseCase usecase.AuthenticationUseCase, rg *gin.Router
 }
 
 func (ac *AuthController) Route() {
-	ac.rg.POST("/signin", ac.login)
+	ac.rg.POST("/signinAdmin", ac.loginAdmin)
+	ac.rg.POST("/signinUser", ac.loginUser)
 }
 
 // Login godoc
-// @Summary Login user
+// @Summary Login Admin User
 // @Description Authenticate user and return a token
 // @Tags auth
 // @Accept json
@@ -33,8 +34,8 @@ func (ac *AuthController) Route() {
 // @Success 200 {object} modelUtil.Response
 // @Failure 400 {object} map[string]string
 // @Failure 401 {object} map[string]string
-// @Router /api/auth/signin [post]
-func (ac *AuthController) login(ctx *gin.Context) {
+// @Router /api/auth/signinAdmin [post]
+func (ac *AuthController) loginAdmin(ctx *gin.Context) {
 	fmt.Println("Starting login process")
 	var payload models.InputLogin
 
@@ -44,10 +45,51 @@ func (ac *AuthController) login(ctx *gin.Context) {
 		return
 	}
 
-	token, user, err := ac.authUseCase.Login(payload.Identifier, payload.Password)
+	token, user, err := ac.authUseCase.LoginAdmin(payload.Identifier, payload.Password)
 
 	if err != nil {
-		if err.Error() == "invalid password" || err.Error() == "user not found" {
+		if err.Error() == "invalid password" || err.Error() == "user not found" || err.Error() == "invalid credentials" {
+			ctx.JSON(http.StatusUnauthorized, modelUtil.APIResponse("Invalid credentials", nil, false))
+			return
+		}
+
+		fmt.Println("Cek err", err.Error())
+		ctx.JSON(http.StatusInternalServerError, modelUtil.APIResponse("Failed to process request", nil, false))
+		return
+	}
+
+	formatedUser := models.FormatUserResponse(user)
+	ctx.JSON(http.StatusOK, modelUtil.APIResponse("success", gin.H{
+		"userPrincipal": formatedUser,
+		"accessToken":   token,
+	}, true))
+}
+
+// Login godoc
+// @Summary Login User
+// @Description Authenticate user and return a token
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param input body models.InputLogin true "Login credentials"
+// @Success 200 {object} modelUtil.Response
+// @Failure 400 {object} map[string]string
+// @Failure 401 {object} map[string]string
+// @Router /api/auth/signinUser [post]
+func (ac *AuthController) loginUser(ctx *gin.Context) {
+	fmt.Println("Starting login process")
+	var payload models.InputLogin
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		fmt.Println("Cek err", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"err": "Invalid input"})
+		return
+	}
+
+	token, user, err := ac.authUseCase.LoginUser(payload.Identifier, payload.Password)
+
+	if err != nil {
+		if err.Error() == "invalid password" || err.Error() == "user not found" || err.Error() == "invalid credentials" {
 			ctx.JSON(http.StatusUnauthorized, modelUtil.APIResponse("Invalid credentials", nil, false))
 			return
 		}
